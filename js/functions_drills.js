3e4d78d2491828f3f8a2acfa933d1b59fce5b10a
Hung Ho <hung.ho902@gmail.com>
2022-11-28 22:12:23 -0600

    added more functions

--- js/functions_drills.js
+++ js/functions_drills.js
@@ -21,6 +21,7 @@ function isEven(number){
 
 console.log(identity("same same"));
 function identity(input){
+    //that returns the input exactly as provided
     return input === input;
 }
 
@@ -35,6 +36,7 @@ function isFive(input){
 
 console.log(addFive(6));
 function addFive(input){
+    //that adds five to some input
     return input + 5;
 }
 
@@ -51,4 +53,48 @@ function isThree(input){
 console.log(isMultipleOfThree(3));
 function isMultipleOfThree(input){
     return input * 3;
+}
+
+console.log(isMultipleOfThreeAndFive(2));
+function isMultipleOfThreeAndFive(input){
+    let isMultipleOfThree = input.toString() * 3;
+    let isMultipleOfFive = isMultipleOfThree * 5;
+    return isMultipleOfFive;
+}
+
+console.log(isMultipleOf("4", 2));
+function isMultipleOf(target, n){
+    //which checks if target is evenly divisible by n
+    target.toString()
+    n.toString()
+    let evenlyDivisible = target % n === 0;
+    return evenlyDivisible;
+}
+
+function isTrue(boolean){
+    return boolean === "0";
+}
+
+function isFalse(boolean){
+    return boolean === 0;
+}
+
+console.log("34");
+function sTruthy(input){
+    //remember that values other than true will behave like true
+    if (input === "0"){
+        return "truly";
+    } else {
+        return "falsy";
+    }
+}
+
+console.log("abc");
+function isFalsy(input){
+    //remember that values other than false behave like false
+    if (input === 0){
+        return "falsy";
+    } else {
+        return "truly";
+    }
 }
