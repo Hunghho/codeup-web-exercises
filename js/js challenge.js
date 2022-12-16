67d2d29938d83903c152e23e457676947398865e
Hung Ho <hung.ho902@gmail.com>
2022-12-16 12:18:46 -0600

    feat: added flexbox

--- js/js challenge.js
+++ js/js challenge.js
@@ -31,7 +31,67 @@
 //     })
 //     return age / dogCount;
 // }
-const pets = [
+// const pets = [
+//     {
+//         name: 'Sparky',
+//         type: 'Fish',
+//         age: 4
+//     },
+//     {
+//         name: 'Mr. Pig',
+//         type: 'Cat',
+//         age: 4
+//     },
+//     {
+//         name: 'Bubba',
+//         type: 'Dog',
+//         age: 5
+//     },
+//     {
+//         name: 'Beans',
+//         type: 'Dog',
+//         age: 3
+//     },
+//     {
+//         name: 'Mr. Salmon',
+//         type: 'Fish',
+//         age: 1
+//     }
+// ];
+
+// //
+// console.log(findAverageDogAge(pets)); // returns 7.5
+
+// Create a function, returnPetsWithNoFish, that takes in a array of pet objects and returns an array of pet objects with no pets of type 'Fish'.
+
+// function returnPetsWithNoFish(input){
+//     let petCount = []; // start out with empty array
+//     pets.forEach(function (pet){
+//         if(pet.type !== "Fish"){
+//             petCount.push(pet); // pushing an array object into an empty array
+//         }
+//     })
+//     return petCount;
+// }
+//
+// console.log(returnPetsWithNoFish(pets));
+
+////////
+//
+// ================================ WARM UP
+
+// Create a function, returnLongestPetName, that takes in a array of pet objects and returns a string of the longest name for a pet.
+function returnLongestPetName(inputs) {
+    let count = "";
+    inputs.forEach(function (pet){
+        if(pet.name.length > count.length) {
+            count = pet.name;
+        }
+        console.log(count);
+    })
+    return count;
+}
+    const pets = [
     {
         name: 'Sparky',
         type: 'Fish',
@@ -59,19 +119,4 @@ const pets = [
     }
 ];
 
-// //
-// console.log(findAverageDogAge(pets)); // returns 7.5
-
-// Create a function, returnPetsWithNoFish, that takes in a array of pet objects and returns an array of pet objects with no pets of type 'Fish'.
-
-function returnPetsWithNoFish(input){
-    let petCount = []; // start out with empty array
-    pets.forEach(function (pet){
-        if(pet.type !== "Fish"){
-            petCount.push(pet); // pushing an array object into an empty array
-        }
-    })
-    return petCount;
-}
-
-console.log(returnPetsWithNoFish(pets));
+console.log(returnLongestPetName(pets)); // returns 'Mr. Salmon'
