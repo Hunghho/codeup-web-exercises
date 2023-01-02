53f09ae5c2f27b7f486381545482b94632779b50
Hung Ho <hung.ho902@gmail.com>
2023-01-02 09:53:44 -0600

    feat:

--- js/js challenge.js
+++ js/js challenge.js
@@ -80,43 +80,85 @@
 //
 // ================================ WARM UP
 
-// Create a function, returnLongestPetName, that takes in a array of pet objects and returns a string of the longest name for a pet.
-function returnLongestPetName(inputs) {
-    let count = "";
-    inputs.forEach(function (pet){
-        if(pet.name.length > count.length) {
-            count = pet.name;
-        }
-        console.log(count);
-    })
-    return count;
-}
-    const pets = [
-    {
-        name: 'Sparky',
-        type: 'Fish',
-        age: 4
-    },
-    {
-        name: 'Mr. Pig',
-        type: 'Cat',
-        age: 4
-    },
-    {
-        name: 'Bubba',
-        type: 'Dog',
-        age: 5
-    },
-    {
-        name: 'Beans',
-        type: 'Dog',
-        age: 3
-    },
-    {
-        name: 'Mr. Salmon',
-        type: 'Fish',
-        age: 1
-    }
-];
+// // Create a function, returnLongestPetName, that takes in a array of pet objects and returns a string of the longest name for a pet.
+// function returnLongestPetName(inputs) {
+//     let count = "";
+//     inputs.forEach(function (pet){
+//         if(pet.name.length > count.length) {
+//             count = pet.name;
+//         }
+//         console.log(count);
+//     })
+//     return count;
+// }
+//     const pets = [
+//     {
+//         name: 'Sparky',
+//         type: 'Fish',
+//         age: 4
+//     },
+//     {
+//         name: 'Mr. Pig',
+//         type: 'Cat',
+//         age: 4
+//     },
+//     {
+//         name: 'Bubba',
+//         type: 'Dog',
+//         age: 5
+//     },
+//     {
+//         name: 'Beans',
+//         type: 'Dog',
+//         age: 3
+//     },
+//     {
+//         name: 'Mr. Salmon',
+//         type: 'Fish',
+//         age: 1
+//     }
+// ];
+//
+// console.log(returnLongestPetName(pets)); // returns 'Mr. Salmon'
 
-console.log(returnLongestPetName(pets)); // returns 'Mr. Salmon'
+
+// Write a function, returnLastTwoChars that returns the last two characters of an input string. Assume a string input of at least two characters.
+// returnLastTwoChars(‘cat’) // returns ‘at’
+// returnLastTwoChars(‘hello’) // returns ‘lo’
+// returnLastTwoChars(‘hi’) // returns ‘hi’
+function returnLastTwoChars(input){
+    if(typeof input === "string"){
+        return input.slice(-2);
+    }
+}
+
+console.log(returnLastTwoChars("hello"));
+
+// EXTRA CHALLENGE
+// Create a function, returnLastCharsReversed that takes in two arguments:
+//     1. a string of characters
+// 2. an integer that represents how many characters should be returned
+// The function should return a string of characters, equal to the number specified
+// by the second argument, in reverse order. If 0 is passed as a second argument,
+// return an empty string. Assume only valid inputs and that the second argument
+// will NOT exceed the length of the input string.
+
+function returnLastCharsReversed(input1, input2){
+    // let splitString = input1.split("");
+    // let reverseArray = splitString.reverse();
+    // let joinArray = reverseArray.join("");
+    // let strSlice = input2.slice();
+    // return joinArray;
+    return input1.split("").reverse().slice(0, input2).join("");
+}
+
+console.log(returnLastCharsReversed("cat", 1));
+
+//     Examples:
+// returnLastCharsReversed(‘cat’, 0) // returns ‘’
+// returnLastCharsReversed(‘cat’, 1) // returns ‘t’
+// returnLastCharsReversed(‘cat’, 2) // returns ‘ta’
+// returnLastCharsReversed(‘cat’, 3) // returns ‘tac’
+// returnLastCharsReversed(‘codeup’, 1) // returns ‘p’
+// returnLastCharsReversed(‘codeup’, 3) // returns ‘pue’
+// returnLastCharsReversed(‘codeup’, 4) // returns ‘pued’
