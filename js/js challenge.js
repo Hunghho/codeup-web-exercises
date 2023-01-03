646ba2fceae309d2e9d619e0bc9c980737259c61
Hung Ho <hung.ho902@gmail.com>
2023-01-03 10:44:09 -0600

    feat:

--- js/js challenge.js
+++ js/js challenge.js
@@ -125,34 +125,34 @@
 // Write a function, returnLastTwoChars that returns the last two characters of an input string. Assume a string input of at least two characters.
 // returnLastTwoChars(‘cat’) // returns ‘at’
 // returnLastTwoChars(‘hello’) // returns ‘lo’
-// returnLastTwoChars(‘hi’) // returns ‘hi’
-function returnLastTwoChars(input){
-    if(typeof input === "string"){
-        return input.slice(-2);
-    }
-}
-
-console.log(returnLastTwoChars("hello"));
-
-// EXTRA CHALLENGE
-// Create a function, returnLastCharsReversed that takes in two arguments:
-//     1. a string of characters
-// 2. an integer that represents how many characters should be returned
-// The function should return a string of characters, equal to the number specified
-// by the second argument, in reverse order. If 0 is passed as a second argument,
-// return an empty string. Assume only valid inputs and that the second argument
-// will NOT exceed the length of the input string.
-
-function returnLastCharsReversed(input1, input2){
-    // let splitString = input1.split("");
-    // let reverseArray = splitString.reverse();
-    // let joinArray = reverseArray.join("");
-    // let strSlice = input2.slice();
-    // return joinArray;
-    return input1.split("").reverse().slice(0, input2).join("");
-}
-
-console.log(returnLastCharsReversed("cat", 1));
+// // returnLastTwoChars(‘hi’) // returns ‘hi’
+// function returnLastTwoChars(input){
+//     if(typeof input === "string"){
+//         return input.slice(-2);
+//     }
+// }
+//
+// console.log(returnLastTwoChars("hello"));
+//
+// // EXTRA CHALLENGE
+// // Create a function, returnLastCharsReversed that takes in two arguments:
+// //     1. a string of characters
+// // 2. an integer that represents how many characters should be returned
+// // The function should return a string of characters, equal to the number specified
+// // by the second argument, in reverse order. If 0 is passed as a second argument,
+// // return an empty string. Assume only valid inputs and that the second argument
+// // will NOT exceed the length of the input string.
+//
+// function returnLastCharsReversed(input1, input2){
+//     // let splitString = input1.split("");
+//     // let reverseArray = splitString.reverse();
+//     // let joinArray = reverseArray.join("");
+//     // let strSlice = input2.slice();
+//     // return joinArray;
+//     return input1.split("").reverse().slice(0, input2).join("");
+// }
+//
+// console.log(returnLastCharsReversed("cat", 1));
 
 //     Examples:
 // returnLastCharsReversed(‘cat’, 0) // returns ‘’
@@ -162,3 +162,29 @@ console.log(returnLastCharsReversed("cat", 1));
 // returnLastCharsReversed(‘codeup’, 1) // returns ‘p’
 // returnLastCharsReversed(‘codeup’, 3) // returns ‘pue’
 // returnLastCharsReversed(‘codeup’, 4) // returns ‘pued’
+
+// Write a function, iBeforeE that takes in a string and returns the string with any ‘ei’ characters replaced with ‘ie’.
+// console.log(iBeforeE(‘ei’)) // returns ‘ie’
+// console.log(iBeforeE(‘height’)) // returns ‘hieght’
+// console.log(iBeforeE(‘heist’)) // returns ‘hiest’
+// console.log(iBeforeE(‘their’)) // returns ‘thier’
+// console.log(iBeforeE(‘theirtheir’)) // returns ‘thierthier’
+
+// function iBeforeE(str){
+//     return str.replaceAll("ei", "ie");
+// }
+
+console.log(iBeforeE("height"))
+
+function iBeforeE(str){
+    let result = "";
+    for (let i = 0 ; i < str.length ; i += 1){
+        if(str[i] + str[i +1] === "ei") {
+            result += "ie";
+            i++;
+        } else {
+            result += str[i];
+        }
+    }
+    return result;
+}
