51c930bee0db9a6c631f5abd9946f7375f0ff6c5
Hung Ho <hung.ho902@gmail.com>
2023-01-13 15:31:22 -0600

    feat:

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,5 +1,16 @@
 "use strict";
 
-$(function (){
-    alert("DOM has finished loading")
-})
+// $(function (){
+//     alert("DOM has finished loading")
+// })
+// $(function() {
+//     alert($('#title').text());
+// });
+// alert($('#title').text());
+// $('.codeup').css('border', '1px solid red');
+
+// $('li').css('font-size', '20px');
+//
+// $('h1, p, li').css('color', 'yellow')
+//
+// alert($('h1').text());
